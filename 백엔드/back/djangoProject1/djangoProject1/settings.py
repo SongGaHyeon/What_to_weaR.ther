cae042fcd077bf901c135c49cdc779159b8258e8
aoi562227 <aoi562227@gmail.com>
2022-05-25 15:49:10 +0900

    link html

--- 백엔드/back/djangoProject1/djangoProject1/settings.py
+++ 백엔드/back/djangoProject1/djangoProject1/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import recommendation.apps
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recommendation.apps.RecommendationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- 백엔드/back/djangoProject1/djangoProject1/urls.py
+++ 백엔드/back/djangoProject1/djangoProject1/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+import recommendation.views
 
 urlpatterns = [
+    path('recommendation/', recommendation.views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
