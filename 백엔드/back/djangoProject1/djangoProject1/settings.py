3a21dd9bd949132cb19aefb581083992e049b4b5
aoi562227 <aoi562227@gmail.com>
2022-05-26 16:31:54 +0900

    0526

--- 백엔드/back/djangoProject1/djangoProject1/settings.py
+++ 백엔드/back/djangoProject1/djangoProject1/settings.py
@@ -125,3 +125,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'lee_yechan@hufs.ac.kr'
+EMAIL_HOST_PASSWORD = '980329'
+SERVER_EMAIL = 'example@email.com'
+DEFAULT_FROM_EMAIL  = 'example@email.com'
