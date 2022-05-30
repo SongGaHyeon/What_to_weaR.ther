ccee8a69a93be05eb19177c143f9a0399b9daccd
serasong <gahyunsong@hufs.ac.kr>
2022-05-30 17:13:02 +0900

    using_selenium : search keyword->상품전체보기

--- 가현님/back/크롤링/seleniumuse.py
+++ 가현님/back/크롤링/seleniumuse.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 browser = webdriver.Chrome('C:/chromedriver.exe')
 browser.get('https://www.musinsa.com/app/')
@@ -10,3 +11,7 @@ search.click()
 
 search.send_keys('가디건')
 search.send_keys(Keys.ENTER)
+
+time.sleep(2)
+products = browser.find_element_by_css_selector('a.btn-all.font-mss')
+products.click()
