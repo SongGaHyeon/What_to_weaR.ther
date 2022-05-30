8a18eb4a125deab94634ad0d475e340a5c1b3117
serasong <gahyunsong@hufs.ac.kr>
2022-05-30 18:05:49 +0900

    using_selenium (가디건) : search keyword > 상품전체보기 > scroll down

--- 가현님/back/크롤링/seleniumuse.py
+++ 가현님/back/크롤링/seleniumuse.py
@@ -4,7 +4,7 @@ import time
 
 browser = webdriver.Chrome('C:/chromedriver.exe')
 browser.get('https://www.musinsa.com/app/')
-browser.implicitly_wait(10)
+browser.implicitly_wait(5)
 
 search = browser.find_element_by_css_selector('input.search.head-search-inp')
 search.click()
@@ -13,5 +13,24 @@ search.send_keys('가디건')
 search.send_keys(Keys.ENTER)
 
 time.sleep(2)
+
+# ..
+browser.execute_script('window.scrollTo(0, 2000)')
+time.sleep(2)
+
 products = browser.find_element_by_css_selector('a.btn-all.font-mss')
 products.click()
+
+
+# scrolling
+before_h = browser.execute_script("return window.scrollY")
+while True:
+    # To END : 전 높이 (before_h) /후 높이
+    browser.find_element_by_css_selector("body").send_keys(Keys.END)
+    time.sleep(1)
+
+    after_h = browser.execute_script("return window.scrollY")
+
+    if after_h == before_h:
+        break
+    before_h = after_h
