d4ef29432daf578c073190f1f7f24a2a6c0d1a69
serasong <gahyunsong@hufs.ac.kr>
2022-06-08 02:13:59 +0900

    back: 0608 : add random change, excel error solve

--- front_back/weatherproject/weatherapp/views.py
+++ front_back/weatherproject/weatherapp/views.py
@@ -20,8 +20,6 @@ if len(thinknit_data) < i:
 else: j = i
 '''
 
-# if len(skirt_data) < i:
-#     j = i % len()
 
 # 4도 이하, 5-8도 -> 가현
 # 9-14도 + 9-14에 맞는 옷 엑셀 배열로 저장하는 부분-> 희원
@@ -394,7 +392,12 @@ def skirt():
         skirt_data.append(line)
 
     f.close()
-    return skirt_data[i]
+
+    if len(skirt_data) < i:
+        j = i % len(skirt_data)
+    else:
+        j = i
+    return skirt_data[j]
 
 
 def summer_pants():
@@ -408,7 +411,12 @@ def summer_pants():
         summerpants_data.append(line)
 
     f.close()
-    return summerpants_data[i]
+
+    if len(summerpants_data) < i:
+        j = i % len(summerpants_data)
+    else:
+        j = i
+    return summerpants_data[j]
 
 
 def short_sleeve():
@@ -422,7 +430,12 @@ def short_sleeve():
         shortsleeve_data.append(line)
 
     f.close()
-    return shortsleeve_data[i]
+
+    if len(shortsleeve_data) < i:
+        j = i % len(shortsleeve_data)
+    else:
+        j = i
+    return shortsleeve_data[j]
 
 
 def short_shirts():
@@ -435,7 +448,12 @@ def short_shirts():
         shortshirts_data.append(line)
 
     f.close()
-    return shortshirts_data[i]
+
+    if len(shortshirts_data) < i:
+        j = i % len(shortshirts_data)
+    else:
+        j = i
+    return shortshirts_data[j]
 
 
 def shorts():
@@ -449,7 +467,12 @@ def shorts():
         shorts_data.append(line)
 
     f.close()
-    return shorts_data[i]
+
+    if len(shorts_data) < i:
+        j = i % len(shorts_data)
+    else:
+        j = i
+    return shorts_data[j]
 
 
 def sleeveless():
@@ -463,7 +486,12 @@ def sleeveless():
         sleeveless_data.append(line)
 
     f.close()
-    return sleeveless_data[i]
+
+    if len(sleeveless_data) < i:
+        j = i % len(sleeveless_data)
+    else:
+        j = i
+    return sleeveless_data[j]
 
 
 def dress():
@@ -477,7 +505,12 @@ def dress():
         dress_data.append(line)
 
     f.close()
-    return dress_data[i]
+
+    if len(dress_data) < i:
+        j = i % len(dress_data)
+    else:
+        j = i
+    return dress_data[j]
 
 
 def shirts():
@@ -490,7 +523,12 @@ def shirts():
         shirts_data.append(line)
 
     f.close()
-    return shirts_data[i]
+
+    if len(shirts_data) < i:
+        j = i % len(shirts_data)
+    else:
+        j = i
+    return shirts_data[j]
 
 
 def long_sleeve():
@@ -503,7 +541,12 @@ def long_sleeve():
         longsleeve_data.append(line)
 
     f.close()
-    return longsleeve_data[i]
+
+    if len(longsleeve_data) < i:
+        j = i % len(longsleeve_data)
+    else:
+        j = i
+    return longsleeve_data[j]
 
 
 def seven_pants():
@@ -517,7 +560,12 @@ def seven_pants():
         sevenpants_data.append(line)
 
     f.close()
-    return sevenpants_data[i]
+
+    if len(sevenpants_data) < i:
+        j = i % len(sevenpants_data)
+    else:
+        j = i
+    return sevenpants_data[j]
 
 
 def bluejacket():
@@ -530,7 +578,12 @@ def bluejacket():
         bluejacket_data.append(line)
 
     f.close()
-    return bluejacket_data[i]
+
+    if len(bluejacket_data) < i:
+        j = i % len(bluejacket_data)
+    else:
+        j = i
+    return bluejacket_data[j]
 
 
 def thin_knit():
@@ -544,6 +597,11 @@ def thin_knit():
         thinknit_data.append(line)
 
     f.close()
+
+    if len(thinknit_data) < i:
+        j = i % len(thinknit_data)
+    else:
+        j = i
 
     if len(thinknit_data) < i:
         j = i % len(thinknit_data)
@@ -563,7 +621,12 @@ def slacks():
         slacks_data.append(line)
 
     f.close()
-    return slacks_data[i]
+
+    if len(slacks_data) < i:
+        j = i % len(slacks_data)
+    else:
+        j = i
+    return slacks_data[j]
 
 
 def cotton_pants():
@@ -577,7 +640,12 @@ def cotton_pants():
         cottonpants_data.append(line)
 
     f.close()
-    return cottonpants_data[i]
+
+    if len(cottonpants_data) < i:
+        j = i % len(cottonpants_data)
+    else:
+        j = i
+    return cottonpants_data[j]
 
 
 def sweatshirt():
@@ -591,7 +659,12 @@ def sweatshirt():
         sweatshirt_data.append(line)
 
     f.close()
-    return sweatshirt_data[i]
+
+    if len(sweatshirt_data) < i:
+        j = i % len(sweatshirt_data)
+    else:
+        j = i
+    return sweatshirt_data[j]
 
 
 def cardigan():
@@ -605,7 +678,12 @@ def cardigan():
         cardigan_data.append(line)
 
     f.close()
-    return cardigan_data[i]
+
+    if len(cardigan_data) < i:
+        j = i % len(cardigan_data)
+    else:
+        j = i
+    return cardigan_data[j]
 
 
 def airlinejumper():
@@ -619,7 +697,12 @@ def airlinejumper():
         airlinejumper_data.append(line)
 
     f.close()
-    return airlinejumper_data[i]
+
+    if len(airlinejumper_data) < i:
+        j = i % len(airlinejumper_data)
+    else:
+        j = i
+    return airlinejumper_data[j]
 
 
 def trenchcoat():
@@ -633,7 +716,12 @@ def trenchcoat():
         trenchcoat_data.append(line)
 
     f.close()
-    return trenchcoat_data[i]
+
+    if len(trenchcoat_data) < i:
+        j = i % len(trenchcoat_data)
+    else:
+        j = i
+    return trenchcoat_data[j]
 
 
 def jean():
@@ -647,7 +735,12 @@ def jean():
         jean_data.append(line)
 
     f.close()
-    return jean_data[i]
+
+    if len(jean_data) < i:
+        j = i % len(jean_data)
+    else:
+        j = i
+    return jean_data[j]
 
 
 def fieldjumper():
@@ -661,7 +754,12 @@ def fieldjumper():
         fieldjumper_data.append(line)
 
     f.close()
-    return fieldjumper_data[i]
+
+    if len(fieldjumper_data) < i:
+        j = i % len(fieldjumper_data)
+    else:
+        j = i
+    return fieldjumper_data[j]
 
 
 def knit():
@@ -675,7 +773,12 @@ def knit():
         knit_data.append(line)
 
     f.close()
-    return knit_data[i]
+
+    if len(knit_data) < i:
+        j = i % len(knit_data)
+    else:
+        j = i
+    return knit_data[j]
 
 
 def furhood():
@@ -689,7 +792,12 @@ def furhood():
         furhood_data.append(line)
 
     f.close()
-    return furhood_data[i]
+
+    if len(furhood_data) < i:
+        j = i % len(furhood_data)
+    else:
+        j = i
+    return furhood_data[j]
 
 
 def fleece():
@@ -702,7 +810,12 @@ def fleece():
         fleece_data.append(line)
 
     f.close()
-    return fleece_data[i]
+
+    if len(fleece_data) < i:
+        j = i % len(fleece_data)
+    else:
+        j = i
+    return fleece_data[j]
 
 
 def shortpadding():
@@ -715,7 +828,12 @@ def shortpadding():
         shortpadding_data.append(line)
 
     f.close()
-    return shortpadding_data[i]
+
+    if len(shortpadding_data) < i:
+        j = i % len(shortpadding_data)
+    else:
+        j = i
+    return shortpadding_data[j]
 
 
 def lightpadding():
@@ -729,7 +847,12 @@ def lightpadding():
         lightpadding_data.append(line)
 
     f.close()
-    return lightpadding_data[i]
+
+    if len(lightpadding_data) < i:
+        j = i % len(lightpadding_data)
+    else:
+        j = i
+    return lightpadding_data[j]
 
 
 def leatherjacket():
@@ -743,7 +866,12 @@ def leatherjacket():
         leatherjacket_data.append(line)
 
     f.close()
-    return leatherjacket_data[i]
+
+    if len(leatherjacket_data) < i:
+        j = i % len(leatherjacket_data)
+    else:
+        j = i
+    return leatherjacket_data[j]
 
 
 def winter_pants():
@@ -757,7 +885,12 @@ def winter_pants():
         pants_data.append(line)
 
     f.close()
-    return pants_data[i]
+
+    if len(pants_data) < i:
+        j = i % len(pants_data)
+    else:
+        j = i
+    return pants_data[j]
 
 # 제목, 링크, 이미지src
 
@@ -778,7 +911,12 @@ def coat():
         coat_data.append(line)
 
     f.close()
-    return coat_data[i]
+
+    if len(coat_data) < i:
+        j = i % len(coat_data)
+    else:
+        j = i
+    return coat_data[j]
 
 
 def muffler():
@@ -792,7 +930,12 @@ def muffler():
         muffler_data.append(line)
 
     f.close()
-    return muffler_data[i]
+
+    if len(muffler_data) < i:
+        j = i % len(muffler_data)
+    else:
+        j = i
+    return muffler_data[j]
 
 
 def glove():
@@ -806,7 +949,12 @@ def glove():
         glove_data.append(line)
 
     f.close()
-    return glove_data[i]
+
+    if len(glove_data) < i:
+        j = i % len(glove_data)
+    else:
+        j = i
+    return glove_data[j]
 
 
 def padding():
@@ -820,7 +968,12 @@ def padding():
         padding_data.append(line)
 
     f.close()
-    return padding_data[i]
+
+    if len(padding_data) < i:
+        j = i % len(padding_data)
+    else:
+        j = i
+    return padding_data[j]
 
 
 def heat():
@@ -834,7 +987,12 @@ def heat():
         heat_data.append(line)
 
     f.close()
-    return heat_data[i]
+
+    if len(heat_data) < i:
+        j = i % len(heat_data)
+    else:
+        j = i
+    return heat_data[j]
 
 
 # ▼ 자기가 맡은 온도에 있는 옷 같이 구현 -> 중복된 옷이면 안해도 됨
